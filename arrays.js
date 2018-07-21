9ee7385e8ea270c4ff6954fa8b93fae461655eaa
Ashley Wong <ashwong04@gmail.com>
2018-07-21 13:34:23 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,33 @@
+var chocolateBars = [
+  'snickers', 'hundred grand', 'kitkat', 'skittles'
+  ]
+  
+  function addElementToBeginningOfArray(arr, ele){
+    let myArray = [ele,...arr];
+    return myArray;
+  }
+  
+  function destructivelyAddElementToBeginningOfArray(arr, ele){
+    arr.unshift(ele);
+    return arr;
+  }
+  
+  function addElementToEndOfArray(arr, ele){
+    let myArray = [arr,...ele];
+    return myArray;
+  }
+
+ function destructivelyAddElementToEndOfArray(arr, ele){
+    arr.push(ele);
+    return arr;
+  }
+  
+  
+  function accessElementInArray(arr, ind){
+    return arr[ind]
+  }
+  
+  
+  
+  
+  
